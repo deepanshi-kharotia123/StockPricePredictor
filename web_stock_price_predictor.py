01a174e18e25317eb94240b48f1873b72a9ac658
Deep <kharotiadeepanshi@gmail.com>
2024-10-16 00:30:38 +0530

    commit

--- web_stock_price_predictor.py
+++ web_stock_price_predictor.py
@@ -15,7 +15,7 @@ google_data=yf.download(stock, start, end)
 
 
 model= load_model("Latest_stock_price_model.keras") 
-st.subheader("Stock Data")
+st.subheader("Stock Data (yfinance)")
 st.write(google_data)
 
 splitting_len= int(len(google_data)*0.7)
@@ -42,7 +42,6 @@ google_data['MA_for_100_days']=google_data.Close.rolling(100).mean()
 st.pyplot(plot_graph((15,6), google_data['MA_for_100_days'],google_data,0))
 
 st.subheader('Original Close Price and MA for 100 days and MA for 250 days')
-# google_data['MA_for_200_days']=google_data.Close.rolling(250).mean()
 st.pyplot(plot_graph((15,6), google_data['MA_for_100_days'],google_data,1,google_data['MA_for_250_days']))
 
 from sklearn.preprocessing import MinMaxScaler
@@ -78,3 +77,34 @@ fig = plt.figure(figsize=(15,6))
 plt.plot(pd.concat([google_data.Close[:splitting_len+100],ploting_data], axis=0))
 plt.legend(["Data- not used", "Original Test data", "Predicted Test data"])
 st.pyplot(fig)
+
+
+
+st.subheader("Future Stock Price Predictions")
+
+num_future_days = 30
+
+last_100_days = scaled_data[-100:]
+future_predictions = []
+
+for i in range(num_future_days):
+    X_future = np.reshape(last_100_days, (1, last_100_days.shape[0], 1))
+    future_price = model.predict(X_future)
+    future_predictions.append(future_price[0, 0])
+
+    last_100_days = np.append(last_100_days[1:], future_price, axis=0)
+
+future_predictions = scaler.inverse_transform(np.array(future_predictions).reshape(-1, 1))
+
+future_dates = pd.date_range(google_data.index[-1], periods=num_future_days)
+
+future_data = pd.DataFrame(future_predictions, index=future_dates, columns=['Predicted Close'])
+
+fig1 = plt.figure(figsize=(15,6))
+plt.plot(future_data.index, future_data['Predicted Close'], color='green', linestyle='-', marker='o')  
+plt.xlabel('Date')
+plt.ylabel('Predicted Close Price')
+plt.title('Predicted Future Stock Prices')
+st.pyplot(fig1)
+
+st.write(future_data)
